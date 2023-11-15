2135ed196bf5d53476668945767da34585f184f3
VISHAL SARAIWAL <vishalsaraiwal68@gmail.com>
2023-11-16 00:21:24 +0530

    routes and controllers

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const product_routes = require("./routes/products")
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.send("Hello World");
 })
 
+// middleware or set the routes
+app.use("/api/products", product_routes);
 
 const start = async () => {
     try{
